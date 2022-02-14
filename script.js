8b947e4d66256283cb5fc07fac800d5547907598
Bartek <kuzniewski.bar@gmail.com>
2022-02-14 16:24:21 +0100

    added function thats counting time in stopwatch

--- script.js
+++ script.js
@@ -12,5 +12,26 @@ const timeList = document.querySelector('.time-list');
 const modelShadow = document.querySelector('.modal-shadow');
 const closeModalBtn = document.querySelector('.close');
 
+let countTime;
+let minutes = 0;
+let seconds = 0;
 
+const handleStart = () => {
+	countTime = setInterval(() => {
+		
+		if (seconds < 9) {
+            seconds++;
+			stopwatch.textContent = `${minutes}:0${seconds}`;
+		} else if (seconds >= 9 && seconds < 59) {
+            seconds++;
+            stopwatch.textContent = `${minutes}:${seconds}`;
+        } else {
+            minutes++
+            seconds = 0
+            stopwatch.textContent = `${minutes}:0${seconds}`
+            
+        }
+	}, 500);
+};
 
+startBtn.addEventListener('click', handleStart);
