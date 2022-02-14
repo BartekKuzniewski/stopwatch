fbaa5c807466fddf92e69c24cd2aacae9f5c113a
Bartek <kuzniewski.bar@gmail.com>
2022-02-14 15:44:57 +0100

    added variables in js

---
+++ script.js
@@ -0,0 +1,16 @@
+const startBtn = document.querySelector('.start');
+const pauseBtn = document.querySelector('.pause');
+const stopBtn = document.querySelector('.stop');
+const resetBtn = document.querySelector('.reset');
+const archiveBtn = document.querySelector('.history');
+const infoIcon = document.querySelector('.fa-question');
+
+const stopwatch = document.querySelector('.stopwatch');
+const timeMeasurement = document.querySelector('.time');
+const timeList = document.querySelector('.time-list');
+
+const modelShadow = document.querySelector('.modal-shadow');
+const closeModalBtn = document.querySelector('.close');
+
+
+
